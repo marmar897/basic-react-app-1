a442f2662cb5c2c41a060676a4af082afe967d6f
Mariana Vazquez <mariana.vazquez7033@gmail.com>
2021-09-20 22:07:53 -0400

    changed link to my github and text to check me out

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/marmar897"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out! 
         </a>
       </header>
     </div>
